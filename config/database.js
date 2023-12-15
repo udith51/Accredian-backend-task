98399811946351ad70e384189014dc7152c98e47
Udith Shyamsukha <udithshyamsukha@gmail.com>
2023-12-15 19:32:13 +0530

    Db deploy

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql")
 const dotenv = require("dotenv");
 dotenv.config();
+console.log(process.env.MYSQL_URL);
 const db = mysql.createConnection(process.env.MYSQL_URL)
 module.exports = db;
